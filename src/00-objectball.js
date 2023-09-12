773c8c18b238c9ad69ff27f9abb89e899aece414
Sackett <skeesen8@gmail.com>
2023-09-12 14:53:52 -0500

    started creating gameObject

---
+++ src/00-objectball.js
@@ -0,0 +1,15 @@
+gameObject(){
+  const  gameObject = {
+    home : {
+        teamName:"Brooklyn Nets",
+        colors:["Black", "White"],
+        players:[]
+    }, 
+    away : {
+        teamName:"Charlotte Hornets",
+        colors:["Turquoise", "Purple"],
+        players:
+    }
+  }
+
+}
