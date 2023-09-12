03a380745b8a45a985e765ed220bd310a840f8c2
Hannah Schwaeble <hannah.schwaeble@gmail.com>
2023-09-12 16:25:39 -0400

    began array nesting

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,15 +1,34 @@
-gameObject(){
-  const  gameObject = {
-    home : {
-        teamName:"Brooklyn Nets",
-        colors:["Black", "White"],
-        players:[]
-    }, 
-    away : {
-        teamName:"Charlotte Hornets",
-        colors:["Turquoise", "Purple"],
-        players:
-    }
-  }
+const  gameObject = {
+  home : {
+      teamName:"Brooklyn Nets",
+      colors:["Black", "White"],
+      players:{
+        'Alan Anderson': {
+          
+        }
 
+      }
+  }, 
+  away : {
+      teamName:"Charlotte Hornets",
+      colors:["Turquoise", "Purple"],
+      players: {
+        'Jeff Adrien': {
+          number: 0,
+          shoe: 16,
+          points:22,
+          rebounds:12,
+          assists:12,
+          steals:3,
+          blocks:1,
+          slamDunks:1,
+        }
+
+      }
+  }
 }
+const 
+
+
+
+console.log(gameObject());
